cdc0face2edd7a2d94cd370723e8d2cbd179a2f7
Skroulex <eshimbekov91@gmail.com>
2022-06-07 11:38:11 +0600

    added post requests

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ GET - получение данных
 команда для запуска json-server
 json-server -w db.json -p 8000
  */
+/*
+CRUD - Create(POST) Read(GET) Update(PUT,PATCH) Delete(DELETE)
+ */
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById('inp-add');
+let btnAdd = document.getElementById('btn-add');
+// console.log(inpAdd, btnAdd)
+btnAdd.addEventListener('click',()=>{
+    let newTodo = {
+        todo: inpAdd.value,
+    };
+    // console.log(newTodo)
+    fetch(API,{
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        headers: {
+            "Content-type": "application/json; charset = utf-8"
+        },
+    });
+});
